8af1701982faec392bf629c8edc58c3bb7112be8
azuressu <martina0130@naver.com>
2024-04-10 15:03:47 +0900

    djano - template practice

---
+++ my_first_pjt/articles/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# 함수형 뷰
+# 클래스형 뷰 - 하나의 클래스 안에 여러 개 함수, 다른 클래스도 상속 가능 (코드를 좀 더 줄일 수 있겠구나)
+def index(request) :
+  # 이 함수로 요청이 들어왔을 때 할 일들을 적어주자
+  response = HttpResponse('<h1>Hello, Django !</h1>')
+  return response
